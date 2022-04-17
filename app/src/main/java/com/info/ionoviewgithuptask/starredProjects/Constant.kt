79144cfd8cd7fbfaec34949973638f545466124d
Abd ElRahman Mosad <development3@teztour.com.eg>
2022-04-17 10:55:36 +0200

    initializing a new task project for the Ionoview

---
+++ app/src/main/java/com/info/ionoviewgithuptask/starredProjects/Constant.kt
@@ -0,0 +1,7 @@
+package com.info.ionoviewgithuptask.starredProjects
+
+object Constant {
+
+    val GITHUP_BASE_URL ="https://api.github.com/search/repositories?q=created:%3E2017-10-22&sort=stars&ord"
+
+}
