9319ede988f49af3bdcb173c457ad8b52e1738fd
darkd <abdelrhmanelgendy243@gmail.com>
2022-04-22 15:18:46 +0200

    creating the ui test for the recycler view and search view

--- app/src/main/java/com/info/ionoviewgithuptask/starredprojects/data/remote/datamodels/gitHupprojects/GithupRepositoryData.kt
+++ app/src/main/java/com/info/ionoviewgithuptask/starredprojects/data/remote/datamodels/gitHupprojects/GithupRepositoryData.kt
@@ -1,7 +1,7 @@
 package com.info.ionoviewgithuptask.starredprojects.data.remote.datamodels
 
 data class GithupRepositoryData(
-    val incomplete_results: Boolean,
-    val items: List<Item>,
-    val total_count: Int
+    val incomplete_results: Boolean?,
+    val items: List<Item>?,
+    val total_count: Int?
 )
